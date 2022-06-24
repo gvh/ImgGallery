5b0580d74d20221c1410e97ea4a6a2dd265b35e9
Gardner von Holt <gardner@wvh.com>
2022-06-23 20:10:24 -0500

    Initial Commit

---
+++ Img Gallery/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Img Gallery
+//
+//  Created by Gardner von Holt on 6/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Img Gallery/Img_GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Img_GalleryApp.swift
+//  Img Gallery
+//
+//  Created by Gardner von Holt on 6/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Img_GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
