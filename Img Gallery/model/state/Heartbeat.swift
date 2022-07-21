dd5425eccf6de3fd12e155c8a8a81aff61843698
Gardner von Holt <gardner@wvh.com>
2022-07-21 17:16:29 -0500

    Add heatbeat class

---
+++ Img Gallery/model/state/Heartbeat.swift
@@ -0,0 +1,15 @@
+//
+//  Heartbeat.swift
+//  ImgGallery
+//
+//  Created by Gardner von Holt on 7/21/22.
+//  Copyright © 2022 Wvh. All rights reserved.
+//
+
+import Foundation
+
+class Heartbeat {
+    static sharedInstance: Heartbeat
+
+    
+}
